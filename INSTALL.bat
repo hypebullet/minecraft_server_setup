f030eed8777cdf55fb69ce792e5e3de79f4ace8f
Zachary Davis <zachary.davis0502@gmail.com>
2022-11-01 00:44:18 -0700

    rename batch file; used appdata env var

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,21 +1,21 @@
 @ECHO OFF
 ECHO Mods folder exists?
-if exist ..\\mods\ (
+if exist %AppData%\\.minecraft\\mods\ (
   ECHO Yes 
-  if exist ..\\mods_archived_serverSetup\ (GOTO :FAILURE)
+  if exist %AppData%\\.minecraft\\mods_archived_serverSetup\ (GOTO :FAILURE)
   ECHO Renaming 'mods' folder to 'mods_archived_serverSetup'
-  rename ..\\mods mods_archived_serverSetup
+  rename %AppData%\\.minecraft\\mods mods_archived_serverSetup
   ECHO DONE
 ) else (
   ECHO No
 )
 
 ECHO Copying client directories!
-robocopy mods ..\\mods
+robocopy mods %AppData%\\.minecraft\\mods
 ECHO 1. Copied Mods!
-robocopy resourcepacks ..\\resourcepacks /E /XC /XN /XO
+robocopy resourcepacks %AppData%\\.minecraft\\resourcepacks /E /XC /XN /XO
 ECHO 2. Copied Resourcepacks!
-robocopy shaderpacks ..\\shaderpacks /E /XC /XN /XO
+robocopy shaderpacks %AppData%\\.minecraft\\shaderpacks /E /XC /XN /XO
 ECHO 3. Copied Shaderpacks!
 
 ECHO ___________________________________________________________________________________________________________________
